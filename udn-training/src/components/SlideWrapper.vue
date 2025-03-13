f8bddce0f3574731b26aaf5fcc6adb2b5e7ad459
Esperanza Cruz Galindo <dulcecrugal@gmail.com>
2025-03-12 23:32:05 -0600

    implementando galeria

---
+++ udn-training/src/components/SlideWrapper.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="slide">
+      <transition name="slide">
+        <slot />
+      </transition>
+    </div>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+  
+  <style scoped>
+  .slide {
+    width: 100%;
+    height: 400px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .slide img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: opacity 0.5s;
+  }
+  
+  .slide-enter-from,
+  .slide-leave-to {
+    opacity: 0;
+  }
+  </style>
+  
