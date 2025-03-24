f4834741625eca5a8c1cf9364da967e5bbeaf273
Orlando Mendez Montes <orlanmen58@gmail.com>
2025-03-24 00:30:09 -0600

    Upd: Eliminando carpetas inecesarias, y agregando estilos globales.

--- udn-training/src/router/index.js
+++ udn-training/src/router/index.js
@@ -21,8 +21,10 @@ import MisEjercicios from '@/components/TrainingAdmin/MisEjercicios.vue';
 import MisDatos from '@/components/TrainingCliente/MisDatos.vue';
 import MiExpediente from '@/components/TrainingCliente/MiExpediente.vue';
 
+
 import ProgresoUsuario from '@/components/ProgresoUsuario.vue'; // Importa el componente correctamente
 const routes = [
+  
   { path: '/progreso', name: 'ProgresoUsuario', component: ProgresoUsuario }, // Ruta para la gráfica de progreso
   { path: '/dietas', name: 'Dietas', component: DietasView }, // Ruta para dietas
   { path: '/', name: 'Home', component: HomeView },
